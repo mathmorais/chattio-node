785a23caee6584cea6b1816c9d62b530e5886201
mathmorais <gm80648@gmail.com>
2022-01-27 16:29:08 -0300

    Just change the indentation

--- lib/main/controllers/UserFriendAcceptController.ts
+++ lib/main/controllers/UserFriendAcceptController.ts
@@ -1,22 +1,22 @@
 import { IUserFriendAcceptService } from "../../core/interfaces/services/IUserFriendAcceptService";
 import { Request, Response } from "express";
 export class UserFriendAcceptController {
-  private service: IUserFriendAcceptService;
+	private service: IUserFriendAcceptService;
 
-  constructor(service: IUserFriendAcceptService) {
-    this.service = service;
-  }
+	constructor(service: IUserFriendAcceptService) {
+		this.service = service;
+	}
 
-  execute = async (req: Request, res: Response): Promise<Response> => {
-    try {
-      const { userId, friendId } = req.body;
+	execute = async (req: Request, res: Response): Promise<Response> => {
+		try {
+			const { userId, friendId } = req.body;
 
-      await this.service.handleAcceptFriend(userId, friendId);
+			await this.service.handleAcceptFriend(userId, friendId);
 
-      return res.status(200).json({ code: "friend.accepted" });
-    } catch (err) {
-      const errorCode = err instanceof Error ? err.message : "error.null";
-      return res.status(401).json({ code: errorCode });
-    }
-  };
+			return res.status(200).json({ code: "friend.accepted" });
+		} catch (err) {
+			const errorCode = err instanceof Error ? err.message : "error.null";
+			return res.status(401).json({ code: errorCode });
+		}
+	};
 }
