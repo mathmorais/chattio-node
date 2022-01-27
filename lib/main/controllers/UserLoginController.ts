eb5a7ea8e293a904fe422850ba1e2f426fecfb3e
mathmorais <gm80648@gmail.com>
2022-01-27 16:30:03 -0300

    Change an error code and set service scope as private

--- lib/main/controllers/UserLoginController.ts
+++ lib/main/controllers/UserLoginController.ts
@@ -2,19 +2,20 @@ import { Request, Response } from "express";
 import { IUserLoginService } from "../../core/interfaces/services/IUserLoginService";
 
 export class UserLoginController {
-  service: IUserLoginService;
+	private service: IUserLoginService;
 
-  constructor(implementation: IUserLoginService) {
-    this.service = implementation;
-  }
+	constructor(implementation: IUserLoginService) {
+		this.service = implementation;
+	}
 
-  execute = async (req: Request, res: Response): Promise<Response> => {
-    try {
-      const token = await this.service.handleUserLogin(req.body);
-      return res.status(200).send({ token });
-    } catch (err) {
-      const errorCode = err instanceof Error ? err.message : "error.invalid";
-      return res.status(401).send({ errorCode });
-    }
-  };
+	execute = async (req: Request, res: Response): Promise<Response> => {
+		try {
+			const token = await this.service.handleUserLogin(req.body);
+			return res.status(200).send({ token });
+		} catch (err) {
+			const errorCode =
+				err instanceof Error ? err.message : "error.invalid";
+			return res.status(401).send({ errorCode });
+		}
+	};
 }
