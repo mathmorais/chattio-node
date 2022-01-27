9856fb87412bed47bd597a5a53b391f4d8e65376
mathmorais <gm80648@gmail.com>
2022-01-27 16:27:37 -0300

    Change some error codes messages

--- lib/main/controllers/UserFriendRequestController.ts
+++ lib/main/controllers/UserFriendRequestController.ts
@@ -4,25 +4,25 @@ import { IUserFriendRequestService } from "../../core/interfaces/services/IUserF
 import { handleVerifyToken } from "../utils/handleVerifyToken";
 
 export class UserFriendRequestController {
-  private service: IUserFriendRequestService;
+	private service: IUserFriendRequestService;
 
-  constructor(implementation: IUserFriendRequestService) {
-    this.service = implementation;
-  }
+	constructor(implementation: IUserFriendRequestService) {
+		this.service = implementation;
+	}
 
-  execute = async (req: Request, res: Response): Promise<Response> => {
-    try {
-      const [_, token] = req.headers.authorization!.split(" ");
+	execute = async (req: Request, res: Response): Promise<Response> => {
+		try {
+			const [_, token] = req.headers.authorization!.split(" ");
 
-      const userId = handleVerifyToken<{ aud: string }>(token).aud;
-      const { friendId } = req.body as IUserFriendRequestDTO;
+			const userId = handleVerifyToken<{ aud: string }>(token).aud;
+			const { friendId } = req.body as IUserFriendRequestDTO;
 
-      await this.service.handleSendFriendRequest(userId, friendId);
+			await this.service.handleSendFriendRequest(userId, friendId);
 
-      return res.json({ code: "friend.send" });
-    } catch (err) {
-      const errorCode = err instanceof Error ? err.message : "error.null";
-      return res.status(400).json({ errorCode });
-    }
-  };
+			return res.json({ code: "friend.requested" });
+		} catch (err) {
+			const errorCode = err instanceof Error ? err.message : "error.unknown";
+			return res.status(400).json({ errorCode });
+		}
+	};
 }
